29ef5aaf89b6400df77a16d1590241eabad82da3
Debiddo <dzheng@terpmail.umd.edu>
2024-11-14 15:55:10 -0500

    created logic to allow computer to generate random number and buttons to allow user to pick higher or lower numbers

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,14 +1,70 @@
-import { Text, View, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import { Text, View, StyleSheet, Alert } from "react-native";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
+import PrimaryButton from "../components/ui/PrimaryButton";
 
-const GameScreen = () => {
+function generateRandomBetween(min, max, exclude) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+let minBoundary = 1;
+let maxBoundary = 100;
+
+const GameScreen = ({ userNumber }) => {
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction) => {
+    // direction => 'lower', 'greater'
+
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+
+    setCurrentGuess(newRndNumber);
+  };
+
   return (
     <View style={styles.screen}>
-      {/* Guess */}
       <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower</Text>
-        {/* text */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </PrimaryButton>
+        </View>
       </View>
       <View>Log Rounds</View>
     </View>
@@ -20,7 +76,6 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 24,
   },
-
 });
 
 export default GameScreen;
